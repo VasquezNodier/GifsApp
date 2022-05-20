0e256fde016cd27559502c3cb30b39ca6a68f503
VasquezNodier <vasqueznap@gmail.com>
2022-05-20 10:46:37 -0500

    Se utiliza el API key de GIPHY para hacer una peticion y se utiliza el modulo http de angular para no usar fetch de js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 
@@ -11,8 +13,10 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule, //Este es el módulo que nos permite hacer consultas de http
     SharedModule,
-    GifsModule
+    GifsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,12 +6,18 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'MXnupyFjAfz6R1EC2Uo73RrczGTF11P8';
+
   private _historial: string[] = []
 
   get historial() {
     return [...this._historial];
     
   }
+
+  // Inyectamos el módulo y podemos hacer peticiones HTTP 
+  // que funciona con base en observables (Son mas poderosos que las promesas)
+  constructor (private http: HttpClient){}
 
   buscarGifs(query:string){
 
@@ -20,6 +27,22 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
     }
+
+    // Esta es una forma de poder consultar los datos, pero es un tanto incómodo por la complejidad
+    // Pero angular ya nos ofrece una opción para hacer consultas HTTP
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=MXnupyFjAfz6R1EC2Uo73RrczGTF11P8&q=dragon ball&limit=10')
+    // .then( resp => {
+    //   resp.json().then( data => {
+    //     console.log(data);
+        
+    //   })
+    // });
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=MXnupyFjAfz6R1EC2Uo73RrczGTF11P8&q=dragon ball&limit=10')
+    .subscribe( (resp:any) => {
+      console.log(resp.data);
+      
+    });
     
     console.log(this._historial)
     
